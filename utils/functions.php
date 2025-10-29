a0fd48c491b84128aeee421faf5bcbc378f2f83a
Rian Vilanova <rianvlnv@gmail.com>
2025-10-29 17:15:59 -0300

    feat: adicionar funções do CRUD de notícias

---
+++ utils/functions.php
@@ -0,0 +1,32 @@
+<?php
+function listarNoticias($conn)
+{
+    $sql = "SELECT * FROM noticias ORDER BY id DESC";
+    $result = $conn->query($sql);
+    return $result;
+}
+
+function adicionarNoticia($conn, $titulo, $subtitulo, $conteudo, $autor, $categoria, $tipo) {
+    $stmt = $conn->prepare("INSERT INTO noticias (titulo, subtitulo, conteudo, autor, categoria, tipo) VALUES (?, ?, ?, ?, ?, ?)"); // Data é atribúida automaticamente pelo banco, nao precisa ser passado aqui
+    $stmt->bindParam("ssssss", $titulo, $subtitulo, $conteudo, $autor, $categoria, $tipo);
+    return $stmt->execute();
+}
+function buscarNoticiaPorId($conn, $id) { // Para auxiliar na funcao de deletar e numa possivel feature de pesquisa por ID
+    $stmt = $conn->prepare("SELECT * FROM noticias WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    return $stmt->get_result()->fetch_assoc();
+}
+
+function atualizarNoticia($conn, $id, $titulo, $subtitulo, $conteudo, $autor, $categoria, $tipo) {
+    $stmt = $conn->prepare("UPDATE noticias SET titulo=?, subtitulo=?, conteudo=?, autor=?, categoria=?, tipo=? WHERE id=?");
+    $stmt->bind_param("ssssssi", $titulo, $subtitulo, $conteudo, $autor, $categoria, $tipo, $id);
+    return $stmt->execute();
+}
+
+function deletarNoticia($conn, $id) {
+    $stmt = $conn->prepare("DELETE FROM noticias WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    return $stmt->execute();
+}   
+?>
